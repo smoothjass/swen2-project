7360c2f769124ae3285ef53cb713dc9d10af9ef0
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-02-24 15:42:30 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fhtw.swen2.duelli.duvivie.swen2project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens fhtw.swen2.duelli.duvivie.swen2project to javafx.fxml;
+    exports fhtw.swen2.duelli.duvivie.swen2project;
+}
