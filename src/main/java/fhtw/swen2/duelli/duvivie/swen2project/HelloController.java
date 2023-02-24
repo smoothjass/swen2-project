a9a3a44bea989f91e10bc6fee3a93089ba5ac992
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-02-24 15:48:15 +0100

    test commit

--- src/main/java/fhtw/swen2/duelli/duvivie/swen2project/HelloController.java
+++ src/main/java/fhtw/swen2/duelli/duvivie/swen2project/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX Application! Some change to check if I can push this to remote.");
     }
 }
