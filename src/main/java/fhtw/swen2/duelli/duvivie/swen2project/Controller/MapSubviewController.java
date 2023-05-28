c8e30e25d60f27db0d5fbb38e0f2736bb714511b
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-05-28 14:31:53 +0200

    es war sehr schön es hat mich sehr gefreut. binding mapsubview funktioniert nicht.

--- src/main/java/fhtw/swen2/duelli/duvivie/swen2project/Controller/MapSubviewController.java
+++ src/main/java/fhtw/swen2/duelli/duvivie/swen2project/Controller/MapSubviewController.java
@@ -2,23 +2,17 @@ package fhtw.swen2.duelli.duvivie.swen2project.Controller;
 
 import fhtw.swen2.duelli.duvivie.swen2project.Entities.Tour;
 import fhtw.swen2.duelli.duvivie.swen2project.Models.MapSubviewModel;
-import fhtw.swen2.duelli.duvivie.swen2project.Services.MapService;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.ExecutionException;
 
 public class MapSubviewController implements Initializable {
+    @FXML
+    public TextField test;
     @FXML
     public ImageView imageView;
     private MapSubviewModel mapSubviewModel;
@@ -27,13 +21,19 @@ public class MapSubviewController implements Initializable {
         this.mapSubviewModel = mapSubviewModel;
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
+
 
     public void requestImage(Tour item) {
         // none of these lines work
         //imageView.setImage(this.mapSubviewModel.requestImage(item));
-        imageView = new ImageView(this.mapSubviewModel.requestImage(item));
+        this.mapSubviewModel.requestImage(item);
+        System.out.println(this.imageView); // this is null. WHY IS THIS NULL?!
+        System.out.println(this.test); // this is null. WHY IS THIS NULL?!
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.test.textProperty().bindBidirectional(mapSubviewModel.getTest());
+        this.imageView.imageProperty().bindBidirectional(mapSubviewModel.getImageView());
     }
 }
